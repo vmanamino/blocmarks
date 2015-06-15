55433b7971f5873e04a52e144457ba3d35e8e3a4
Viral Amin <vmanamino@gmail.com>
2015-06-15 20:48:27 +0000

    incoming emails controller"

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,22 +5,29 @@ class IncomingController < ApplicationController
   
   def create
     puts "INCOMING PARAMS HERE #{params}"
-    @name = params[:sender]
+    @from = params[:from]
+    @user = User.find_by(email: @from)
+    if @user.nil?
+      @user = User.new(params.require(:user).permit(:email))
+      @user.email = @from
+      @user.skip_confirmation!
+      @user.save!
+    end
     @subject = params[:subject]
+    @topic = Topic.find_by(title: @subject)
+    if @topic.nil?
+      @topic = Topic.new(params.require(:topic).permit(:title, :user))
+      @topic.title = @subject
+      @topic.user = @user
+      @topic.save!            
+    end
+    
     @url = params['body-plain']
-    
-    if @name.nil?
-      @name = "viral"
-    end
-    
-    if @subject.nil?
-      @subject = 'random musings'
-    end
-    
-    @bookmark = Bookmark.new
+    @bookmark = Bookmark.new(params.require(:bookmark).permit(:url, :topic))
     @bookmark.url = @url
-    @bookmark.topic = @subject
+    @bookmark.topic = @topic
     @bookmark.save
+    
     head 200
   end
 end
