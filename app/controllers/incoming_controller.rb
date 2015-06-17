a12fd2e9b8956081035cb128d22a68ff96bcd30b
Viral Amin <vmanamino@gmail.com>
2015-06-17 02:23:37 +0000

    modified incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,8 +3,8 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    user = User.find_by(email: params[:sender])
-    create_user unless user
+    @user = User.find_by(email: params[:sender])
+    create_user unless @user
     topic = Topic.find_by(title: params[:subject])
     create_topic unless topic
     bookmark = Bookmark.new(url: params['body-plain'], topic: topic)
@@ -13,13 +13,13 @@ class IncomingController < ApplicationController
   end
 
   def create_user
-    user = User.new(email: params[:sender], password: params[:sender])
-    user.skip_confirmation!
-    user.save!(validate: false)
+    @user = User.new(email: params[:sender], password: params[:sender])
+    @user.skip_confirmation!
+    @user.save!(validate: false)
   end
 
   def create_topic
-    topic = Topic.new(title: params[:subject], user: params[:sender])
+    topic = Topic.new(title: params[:subject], user: @user)
     topic.save!
   end
 end
