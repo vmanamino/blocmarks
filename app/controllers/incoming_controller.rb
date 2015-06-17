9460ec5958ab88b1c827e6bf4a42d8d0e45f6424
Viral Amin <vmanamino@gmail.com>
2015-06-17 02:03:23 +0000

    modified incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,30 +3,23 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    @from = params[:sender]
-    @user = User.find_by(email: @from)
-    if @user.nil?
-      @user = User.new
-      @user.email = @from
-      @user.encrypted_password = '$2a$10$5eoHh6M2q4GjGkHClO.NqebWWhS94D8rNj5Ot6CB2qrbn7IrTfkSa'
-      @user.skip_confirmation!
-      @user.save!(validate: false)
-    end
-    @subject = params[:subject]
-    @topic = Topic.find_by(title: @subject)
-    if @topic.nil?
-      @topic = Topic.new
-      @topic.title = @subject
-      @topic.user = @user
-      @topic.save!
-    end
-
-    @url = params['body-plain']
-    @bookmark = Bookmark.new
-    @bookmark.url = @url
-    @bookmark.topic = @topic
-    @bookmark.save!
-
+    user = User.find_by(email: params[:sender])
+    create_user unless user
+    topic = Topic.find_by(title: params[:subject])
+    create_topic unless topic
+    bookmark = Bookmark.new(url: params['body-plain'], topic: topic)
+    bookmark.save!
     head 200
+  end
+
+  def create_user
+    user = User.new(email: params[:sender], password: params[:sender])
+    user.skip_confirmation!
+    user.save!(validate: false)
+  end
+
+  def create_topic
+    topic = Topic.new(title: params[:subject], user: user)
+    topic.save!
   end
 end
