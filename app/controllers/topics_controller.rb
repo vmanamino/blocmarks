aea3f61272c3669db8210e6a7d3425d5dc193d65
Viral Amin <vmanamino@gmail.com>
2015-06-18 18:53:58 +0000

    topic create in progress

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,6 +8,20 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+    # authorize @topic
+  end
+  
+  def create
+    @topic = Topic.new(params.require(:topic).permit(:title))
+    # authorize @topic
+   
+    if @topic.save
+      redirect_to @topic, notice: 'Topic was saved successfully.'
+    else
+      flash[:error] = 'There was an error saving your topic.  Please try again.'
+      render :new
+    end
   end
 
   def edit
