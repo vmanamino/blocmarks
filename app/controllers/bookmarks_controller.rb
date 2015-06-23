1b778bef6a0fcd4179873e797f65d77045c216ac
Viral Amin <vmanamino@gmail.com>
2015-06-23 02:52:16 +0000

    implemented show view, policy sensitive links

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,10 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
   def show
     @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
+    @topic = @bookmark.topic
   end
 
   def new
@@ -8,8 +12,6 @@ class BookmarksController < ApplicationController
     authorize @bookmark
     @topic = Topic.find(params[:topic_id])
   end
-
-  before_action :authenticate_user!
 
   def create
     @bookmark = current_user.bookmarks.build(params.require(:bookmark).permit(:url))
@@ -45,9 +47,9 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
     authorize @bookmark
     if @bookmark.update_attributes(params.require(:bookmark).permit(:url))
-      redirect_to @topic, notice: 'Your bookmark was updated'
+      redirect_to [@topic, @bookmark], notice: 'Your bookmark was updated'
     else
-      redirec_to @topic, error: 'Your bookmark failed to update, please try again'
+      redirec_to [@topic, @bookmark], error: 'Your bookmark failed to update, please try again'
     end
   end
 
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,6 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @topics = Topic.all
     authorize @topics
@@ -14,7 +16,6 @@ class TopicsController < ApplicationController
     authorize @topic
   end
 
-  # before_action :authenticate_user!  Dalibor, when I use this un-authenticated users canot see the index view, I don't understand why?
   def create
     @topic = current_user.topics.build(params.require(:topic).permit(:title))
     authorize @topic
