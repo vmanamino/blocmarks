09729dcb211e02985988ba00603d586fc53a0397
Viral Amin <vmanamino@gmail.com>
2015-06-26 03:57:47 +0000

    rubocop errors fixed

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,19 +4,19 @@ class LikesController < ApplicationController
     @bookmark = Bookmark.find(params[:bookmark_id])
     @topic = @bookmark.topic
     like = current_user.likes.build(bookmark: @bookmark)
-    
+
     if like.save
       redirect_to [@topic, @bookmark], notice: 'You succesfully liked the bookmark'
     else
-      redirect_to [@topic, @bookmark], error: 'You failed to like the bookmark'      
-    end    
+      redirect_to [@topic, @bookmark], error: 'You failed to like the bookmark'
+    end
   end
-  
+
   def destroy
     @bookmark = Bookmark.find(params[:bookmark_id])
     @topic = @bookmark.topic
     like = Like.find(params[:id])
-    
+
     if like.destroy
       redirect_to [@topic, @bookmark], notice: 'You unliked the bookmark'
     else
