db2f7ed8161a1bdf945c4a23a8dc4d37371fbcc1
Viral Amin <vmanamino@gmail.com>
2015-06-26 05:11:43 +0000

    user-profiles controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
+    @user = current_user
+    @user_bookmarks = @user.bookmarks
+    @user_likes = @user.likes
   end
+  
+  
 end
