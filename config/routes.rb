5f335de81d1dc4de484254f8804a098f31721e0f
Viral Amin <vmanamino@gmail.com>
2015-06-26 03:53:28 +0000

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  
+
   resources :topics do
     resources :bookmarks, except: [:index] do
       resources :likes, only: [:create, :destroy]
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+  resources :users, only: [:show]
+  
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
   
